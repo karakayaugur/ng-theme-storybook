619335edea86793481b74cb671a19ac5eb27898e
ugur cakil <ugur.c@freelancer.run>
2024-12-18 16:09:12 +0000

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "@app/pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+];
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+    selector: 'app-button',
+    imports: [CommonModule],
+    templateUrl: './button.component.html',
+    styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  constructor() {}
+  @Input() isDisabled: boolean = false;
+  @Input() loading: boolean = true;
+  @Input() color: 'default' | 'primary' | 'secondary' | 'danger' | 'success' =
+    'default';
+  @Input() size: 'default' | 'small' | 'large' = 'default';
+  @Input() block: boolean = true;
+  @Input() label: string = 'Button';
+  @Input() radius: string = '0.375rem';
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() fontFace: 'Inter' | 'Manrope' | 'Nunito' | 'Raleway' = 'Inter';
+  @Output() onClick = new EventEmitter<any>();
+
+  handleClick(event: any) {
+    this.onClick.emit(event);
+  }
+}
