95ee6e81682c6bd72f80cf78aacb16413a787d3e
ugur cakil <ugur.c@freelancer.run>
2025-01-01 18:09:49 +0000

    dev

---
+++ src/app/pages/inputs/inputs.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-inputs",
+  imports: [CommonModule],
+  templateUrl: "./inputs.component.html",
+  styleUrl: "./inputs.component.css",
+})
+export class InputsComponent {}
