8f8c0a80e2d13570bd7c4d9f78f6ae4ea9374cfb
ugur cakil <ugur.c@freelancer.run>
2025-11-24 18:15:11 +0000

    main

--- src/app/shared/components/button/button.stories.ts
+++ src/app/shared/components/button/button.stories.ts
@@ -29,10 +29,42 @@ export default meta;
 
 type Story = StoryObj<ButtonComponent>;
 
+// Default
+export const Default: StoryObj<ButtonComponent> = {
+  render: (args) => ({
+    props: { args },
+    template: `
+      <h4>Button default</h4>
+      <div style="display: flex; gap: 1rem;">
+        <app-button [label]="args.label" [variant]="'default'" [size]="'small'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+        <app-button [label]="args.label" [variant]="'default'" [size]="'medium'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+        <app-button [label]="args.label" [variant]="'default'" [size]="'large'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+      </div>
+    `,
+  }),
+};
+
+export const DefaultIcon: StoryObj<ButtonComponent> = {
+  render: (args) => ({
+    props: { args },
+    template: `
+    <h4>Button default icon</h4>
+      <div style="display: flex; gap: 1rem;">
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'default'" [size]="'small'" [loading]="args.loading" [isDisabled]="args.isDisabled">
+        </app-button>
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'default'" [size]="'medium'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'default'" [size]="'large'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+      </div>
+    `,
+  }),
+};
+
+// Primary
 export const Primary: StoryObj<ButtonComponent> = {
   render: (args) => ({
     props: { args },
     template: `
+      <h4>Button primary</h4>
       <div style="display: flex; gap: 1rem;">
         <app-button [label]="args.label" [variant]="'primary'" [size]="'small'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
         <app-button [label]="args.label" [variant]="'primary'" [size]="'medium'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
@@ -42,14 +74,44 @@ export const Primary: StoryObj<ButtonComponent> = {
   }),
 };
 
+export const PrimaryIcon: StoryObj<ButtonComponent> = {
+  render: (args) => ({
+    props: { args },
+    template: `
+      <h4>Button primary icon</h4>
+      <div style="display: flex; gap: 1rem;">
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'primary'" [size]="'small'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'primary'" [size]="'medium'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'primary'" [size]="'large'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+      </div>
+    `,
+  }),
+};
+
+// Destructive
 export const Destructive: StoryObj<ButtonComponent> = {
   render: (args) => ({
     props: { args },
     template: `
+      <h4>Button destructive</h4>
       <div style="display: flex; gap: 1rem;">
         <app-button [label]="args.label" [variant]="'destructive'" [size]="'small'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
         <app-button [label]="args.label" [variant]="'destructive'" [size]="'medium'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
         <app-button [label]="args.label" [variant]="'destructive'" [size]="'large'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+      </div>
+    `,
+  }),
+};
+
+export const DestructiveIcon: StoryObj<ButtonComponent> = {
+  render: (args) => ({
+    props: { args },
+    template: `
+      <h4>Button destructive icon</h4>
+      <div style="display: flex; gap: 1rem;">
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'destructive'" [size]="'small'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'destructive'" [size]="'medium'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
+        <app-button [class]="'button-icon'" [label]="args.label" [variant]="'destructive'" [size]="'large'" [loading]="args.loading" [isDisabled]="args.isDisabled"></app-button>
       </div>
     `,
   }),
